36636340db3b6e9417480a1b5d55aac29369705b
mark <furman.mark7887@gmail.com>
2024-06-14 17:48:57 +0500

    initial commit w/ bot

---
+++ main.py
@@ -0,0 +1,51 @@
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, types, F, Router
+from aiogram.filters.command import Command
+
+logging.basicConfig(level=logging.INFO)
+bot = Bot(token="7169830227:AAFVpoHPO3ZVSJ7eLRyI0DIaidYs7r7e-Ms")
+router = Router()
+dp = Dispatcher()
+GREETINGS_MESSAGE = "Привет! Я могу помочь тебе определить дефектых сварных швов!"
+
+class Processor:
+    @staticmethod
+    def processPhoto():
+            # TODO process photo with AI
+
+    @staticmethod
+    def processVideo():
+            # TODO process video with AI
+
+
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    kb = [
+        [types.KeyboardButton(text="Отлично")]
+    ]
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, keyboard=kb)
+    await message.answer(GREETINGS_MESSAGE, reply_markup=keyboard)
+
+@dp.message(F.text.lower() == "отлично")
+async def chooseInputType(message: types.Message):
+    kb = [
+        [types.KeyboardButton(text="Фото"),
+         types.KeyboardButton(text="Видео")]
+    ]
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, keyboard=kb)
+    await message.answer("Выберите тип ввода данных: фото или видео?", reply_markup=keyboard)
+
+@dp.message()
+async def userInput(message: types.Message):
+    if message.photo:
+        await message.answer("photo")
+    elif message.video:
+        await message.answer("video")
+    else:
+        await message.answer("eblan")
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
